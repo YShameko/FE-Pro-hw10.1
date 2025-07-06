3c22999702838066bbc4eab8dcf2ce80cc518b47
YShameko <yshameko@gmail.com>
2025-07-06 16:11:56 +0300

    hw10.1

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 "use strict";
 
 const userInfo = {
+	userName: 'jsmith',
 	firstName: 'John',
 	lastName: 'Smith',
 	age: 35,
 	gender: 'male',
-	address: 'Київ, а/с 123',
-	getProterties: function (){
-		for (const key in this) {
-			if (typeof this[key] !== 'function') {
-				console.log(`${key}: ${this[key]}`);
-			}
-		}
+	address: { 
+		country: 'Україна',
+		city: 'Київ',
+		postalIndex: '01001',
+		street: 'а/с 123',
 	},
+	accessLevel: 'user',
+	getProterties: function (object = this){ 
+        for (const key in object) {
+			const value = object[key];
+			if (typeof value === 'object' && value !== null) {
+				console.log(`${key}: `); 
+				this.getProterties(value); 
+			}
+			else if (typeof value !== 'function') {
+				console.log(`${key}: ${value}`); 
+			}
+        }
+    },
 };
 
 console.log('Info about the current user:');
